9dc3a005df1fea240d2621dde8837d28b7ad7b4c
안승지 <69342971+s-ja@users.noreply.github.com>
2022-04-27 12:52:10 +0900

    스타일링 완료, js 마지막 slide 부분 오류 수정 필요

--- script/main.js
+++ script/main.js
@@ -1,3 +1,47 @@
 $(function(){
-    
+    board()
+    popup()
+    menu()
+    // slide()
+    let interval_id = setInterval(slide,3000)
 })
+
+function popup(){
+    $(".news li").first().click(function(){
+        $("#popup").removeClass("d_none")
+    })
+    $("#popup button").click(function(){
+        $("#popup").addClass("d_none")
+    })
+}
+
+function board(){
+    $(".board_title li").click(function(){
+        if(!$(this).hasClass("active")){
+            $(this).addClass("active")
+            $(this).siblings().removeClass("active")
+            let idx = $(this).index()
+            $(".board_contents ul").eq(idx).removeClass("d_none")
+            $(".board_contents ul").eq(idx).siblings().addClass("d_none")
+        }
+    })
+}
+
+function menu(){
+    $("#gnb > li").mouseenter(function(){
+        $(this).addClass("mouseenter")
+        $(this).children(".lnb").stop().slideDown()
+    })
+    $("#gnb > li").mouseleave(function(){
+        $(this).removeClass("mouseenter")
+        $(this).children(".lnb").stop().slideUp()
+    })
+}
+
+function slide(){
+    let curr_slide = $("#imgs li.top")
+    let next_slide = curr_slide.next()
+    if(!next_slide.length) next_slide = $("#imgs li:first")
+    curr_slide.removeClass("top")
+    next_slide.addClass("top")
+}
